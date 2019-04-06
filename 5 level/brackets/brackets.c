1a5a7b5611ffeafc47a9b240cc92b28d63e7ca00
Roxane Amory lorch <ramory-l@mi-c6.21-school.ru>
2019-04-06 12:42:00 +0300

    5 level/brackets

--- 5 level/brackets/brackets.c
+++ 5 level/brackets/brackets.c
@@ -11,7 +11,7 @@ int		ft_match(char b1, char b2)
 	return (0);
 }
 
-int	brackets(char *argv, char *stack)
+void	brackets(char *argv, char *stack)
 {
 	int i;
 	int top;
@@ -27,21 +27,23 @@ int	brackets(char *argv, char *stack)
 		}
 		if (argv[i] == ')' || argv[i] == '}' || argv[i] == ']')
 		{
-			if (!stack[top])
-				return (0);
-			else if (ft_match(stack[top], argv[i]))
+			if (ft_match(stack[top], argv[i]))
 			{
 				stack[top] = 0;
 				top--;
 			}
 			else
-				return (0);
+			{
+				write(1, "Error\n", 6);
+				return ;
+			}
 		}
 		i++;
 	}
 	if (stack[0] == 0)
-		return (1);
-	return (0);
+		write(1, "OK\n", 3);
+	else
+		write(1, "Error\n", 6);
 }
 
 int	main(int argc, char *argv[])
@@ -54,10 +56,7 @@ int	main(int argc, char *argv[])
 	{
 		while (argv[i])
 		{
-			if (brackets(argv[i], stack))
-				write(1, "OK\n", 3);
-			else
-				write(1, "Error\n", 6);
+			brackets(argv[i], stack);
 			i++;
 		}
 	}
